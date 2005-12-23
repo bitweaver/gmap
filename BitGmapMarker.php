d1764a48c1438bcf6abf37a48d997750ed03e97a
wjames5 <will@tekimaki.com>
2005-12-23 16:20:02 +0000

    *** empty log message ***

---
+++ BitGmapMarker.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * BitGmapMarker Class
+ *
+ * @package gmap
+ * @subpackage BitGmapMarker
+ *
+ * @author will <will@wjamesphoto.com>
+ *
+ * @version v.0
+ *
+ * Copyright (c) 2005 bitweaver.org, Will James
+ * All Rights Reserved. See copyright.txt for details and a complete list of authors.
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
+ *
+ */
+
+/**
+ * required setup
+ */
+
+require_once( LIBERTY_PKG_PATH.'LibertyAttachable.php' );
+
+/**
+* This is used to uniquely identify the object
+*/
+define( 'BITGMAPMARKER_CONTENT_TYPE_GUID', 'bitgmapmarker' );
+
+
+// this is the class that contains all the functions for the package
+class BitGmapMarker extends LibertyAttachable {
+	/**
+	* Primary key for our map class
+	* @public
+	*/
+	var $mGmarkerId;
+
+
+	/**
+	* During initialisation, be sure to call our base constructors
+	**/
+	function BitGmapMarker( $pGmarkerId=NULL, $pContentId=NULL ) {
+		parent::LibertyAttachable();
+		$this->mGmarkerId = $pGmarkerId;
+		$this->mContentId = $pContentId;
+		$this->mContentTypeGuid = BITGMAPMARKER_CONTENT_TYPE_GUID;
+		$this->registerContentType( BITGMAPMARKER_CONTENT_TYPE_GUID, array(
+			'content_type_guid' => BITGMAPMARKER_CONTENT_TYPE_GUID,
+			'content_description' => 'Markers for the Gmap Package',
+			'handler_class' => 'BitGmapMarker',
+			'handler_package' => 'gmap',
+			'handler_file' => 'BitGmapMarker.php',
+			'maintainer_url' => 'http://www.bitweaver.org'
+		) );
+	}
+
+
+
+	//returns array of marker data and associated style and icon style ids for given gmap_id and set_type
+	function load() {
+		if( !empty( $this->mGmarkerId ) || !empty( $this->mContentId ) ) {
+			// LibertyContent::load()assumes you have joined already, and will not execute any sql!
+			// This is a significant performance optimization
+			$lookupColumn = !empty( $this->mGmarkerId )? 'marker_id' : 'content_id';
+			$lookupId = !empty( $this->mGmarkerId )? $this->mGmarkerId : $this->mContentId;
+
+			$query = "SELECT *
+					  FROM `".BIT_DB_PREFIX."bit_gmaps_markers` bmm INNER JOIN `".BIT_DB_PREFIX."tiki_content` tc ON( bmm.`content_id`=tc.`content_id` )
+					  WHERE bmm.`$lookupColumn`=?";
+			$result = $this->mDb->query( $query, array( $lookupId ) );
+
+			if( $result && $result->numRows() ) {
+				$this->mInfo = $result->fields;
+				$this->mGmarkerId = $result->fields['marker_id'];
+				$this->mContentId = $result->fields['content_id'];
+				parent::load();				
+			}
+		}
+		return( count( $this->mInfo ) );
+	}
+
+	
+	
+	
+	
+
+//ALL STORE FUNCTIONS
+
+	function verify( &$pParamHash ) {
+
+		$pParamHash['marker_store'] = array();
+		
+		if( !empty( $pParamHash['marker_lat'] ) && is_numeric( $pParamHash['marker_lat'] ) ) {
+			$pParamHash['marker_store']['lat'] = $pParamHash['marker_lat'];
+		}
+
+		if( !empty( $pParamHash['marker_lon'] ) && is_numeric( $pParamHash['marker_lon'] ) ) {
+			$pParamHash['marker_store']['lon'] = $pParamHash['marker_lon'];
+		}
+
+		if( !empty( $pParamHash['marker_labeltext'] ) ) {
+			$pParamHash['marker_store']['label_data'] = $pParamHash['marker_labeltext'];
+		}
+
+		if( !empty( $pParamHash['marker_zi'] ) && is_numeric( $pParamHash['marker_zi'] ) ) {
+			$pParamHash['marker_store']['zindex'] = $pParamHash['marker_zi'];
+		}
+		
+		if( !empty( $pParamHash['map_comm'] ) ) {
+			$pParamHash['marker_store']['allow_comments'] = 'TRUE';
+		}
+
+		return( count( $this->mErrors ) == 0 );
+	}
+
+	
+	function store( &$pParamHash ) {
+		if( $this->verify( $pParamHash ) ) {
+			$this->mDb->StartTrans();
+			if( parent::store( $pParamHash ) ) {
+				if( $this->mGmarkerId ) {
+					// store the posted changes
+					$this->mDb->associateUpdate( BIT_DB_PREFIX."bit_gmaps_markers", $pParamHash['marker_store'], array( "name" => "marker_id", "value" => $pParamHash['marker_id'] ) );
+				} else {
+					$pParamHash['marker_store']['content_id'] = $this->mContentId;
+					$this->mDb->associateInsert( BIT_DB_PREFIX."bit_gmaps_markers", $pParamHash['marker_store'] );
+				}
+				$this->mDb->CompleteTrans();
+
+				// re-query to confirm results
+				$result = $this->load();
+
+			} else {
+				$this->mDb->RollbackTrans();
+			}
+		}
+		return( count( $this->mInfo ) );
+	}
+
+}
+
+?>
---
+++ edit_polyline.php
@@ -0,0 +1,65 @@
+<?php
+// Copyright (c) 2005 bitweaver Gmap
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+
+// Initialization
+require_once('../bit_setup_inc.php' );
+
+// Is package installed and enabled
+$gBitSystem->verifyPackage('gmap' );
+
+// Now check permissions to access this page
+$gBitSystem->verifyPermission('bit_gm_edit_map' );
+
+// Access the gmap class
+global $gContent;
+require_once( GMAP_PKG_PATH.'BitGmap.php');
+require_once( LIBERTY_PKG_PATH.'lookup_content_inc.php' );
+$gContent = new BitGmap();
+
+//Preview mode is handled by javascript on the client side.
+//There is no callback to the server for previewing changes.
+
+//Check if this is a update or a new map
+if (!empty($_REQUEST["save_polyline"])) {
+    if( $gContent->storePolyline( $_REQUEST ) ) {		
+				//if store is successful we return XML
+				$mRet = "<polyline>"
+      			  ."<id>".$result->fields['polyline_id']."</id>"
+              ."<name>".$result->fields['name']."</name>"
+              ."<type>".$result->fields['type']."</type>"
+              ."<points>".$result->fields['points_data']."</points>"
+              ."<border>".$result->fields['border_text']."</border>"
+              ."<z>".$result->fields['zindex']."</z>"
+						  ."</polyline>";
+
+				//@todo replace in xml when I know what I am doing
+				//	."<data>".$gContent->parseData()."</data>"
+					
+      	//since we are returning xml we must report so in the header
+      	//we also need to tell the browser not to cache the page
+      	//see: http://mapki.com/index.php?title=Dynamic_XML
+        // Date in the past
+        header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); 
+        // always modified
+        header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+        // HTTP/1.1
+        header("Cache-Control: no-store, no-cache, must-revalidate");
+        header("Cache-Control: post-check=0, pre-check=0", false);
+        // HTTP/1.0
+        header("Pragma: no-cache");
+        //XML Header
+        header("content-type:text/xml");
+     		
+      	print_r($mRet);
+
+        die;
+								
+    }else{
+		//@todo - return some sort of store failure message in the xml
+      $gBitSmarty->assign_by_ref('errors', $gContent->mErrors );
+    }
+}
+
+?>	
