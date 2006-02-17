9d2d5365e1b7a918536f4e2db4c909c2b5c38ddb
Max Kremmel <xing@synapse.plus.com>
2006-02-17 23:51:42 +0000

    registerPackage now takes a hash instead of multiple parameters - keeps # of parameters under control

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -1,17 +1,22 @@
 <?php
-	global $gBitSystem, $gBitUser, $gBitSmarty;
-	$gBitSystem->registerPackage( 'gmap', dirname( __FILE__).'/' );
+global $gBitSystem, $gBitUser, $gBitSmarty;
 
-	if($gBitSystem->isPackageActive( 'gmap' ) ) {
-		if ($gBitUser->hasPermission( 'bit_gm_view_map' )) {
-			$gBitSystem->registerAppMenu( GMAP_PKG_DIR, 'Gmap', GMAP_PKG_URL.'index.php', 'bitpackage:gmap/menu_gmap.tpl', 'gmap');
-		}
+$registerHash = array(
+	'package_name' => 'gmap',
+	'package_path' => dirname( __FILE__ ).'/',
+);
+$gBitSystem->registerPackage( $registerHash );
 
-		$gmapHomePage = $gBitSystem->getPreference("gmapHomePage", 'HomePage');
-		//@todo wj: does this cause conflict with wiki or are these package independent and therefor can have same name?
-		$anonCanEdit = $gBitSystem->getPreference("anonCanEdit", 'n');
-		$gBitSmarty->assign('anonCanEdit', $anonCanEdit);
-		$gBitSmarty->assign('gmapHomePage', $gmapHomePage);
+if($gBitSystem->isPackageActive( 'gmap' ) ) {
+	if ($gBitUser->hasPermission( 'bit_gm_view_map' )) {
+		$gBitSystem->registerAppMenu( GMAP_PKG_DIR, 'Gmap', GMAP_PKG_URL.'index.php', 'bitpackage:gmap/menu_gmap.tpl', 'gmap');
 	}
+
+	$gmapHomePage = $gBitSystem->getPreference("gmapHomePage", 'HomePage');
+	//@todo wj: does this cause conflict with wiki or are these package independent and therefor can have same name?
+	$anonCanEdit = $gBitSystem->getPreference("anonCanEdit", 'n');
+	$gBitSmarty->assign('anonCanEdit', $anonCanEdit);
+	$gBitSmarty->assign('gmapHomePage', $gmapHomePage);
+}
 
 ?>
