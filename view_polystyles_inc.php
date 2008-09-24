e3e7936d5a906bbf46c05db947c6b29fe1d0e63b
wjames5 <will@tekimaki.com>
2008-09-24 15:53:35 +0000

    fix hash ref

--- view_polystyles_inc.php
+++ view_polystyles_inc.php
@@ -12,15 +12,17 @@ $gBitSystem->verifyPermission( 'p_gmap_view' );
 //if a style id is passed try to look it up
 if( @BitBase::verifyId( $_REQUEST['polylinestyle_id'] )) {
 
-	$getStyleFunc = $gContent->getPolylineStyle;
+	$getStyleFunc = 'getPolylineStyle';
 	$styleId = $_REQUEST['polylinestyle_id'];
-	$tpl = 'bitpackage:gmap/polylinestyle_xml.tpl';
+	$tpl = 'bitpackage:gmap/edit_polylinestyle_xml.tpl';
+	$styleHashName = 'polylinestyleInfo';
 	
 }elseif( @BitBase::verifyId( $_REQUEST['polygonstyle_id'] )) {
 
-	$getStyleFunc = $gContent->getPolygonStyle;
+	$getStyleFunc = 'getPolygonStyle';
 	$styleId = $_REQUEST['polylinestyle_id'];
-	$tpl = 'bitpackage:gmap/polygonstyle_xml.tpl';
+	$tpl = 'bitpackage:gmap/edit_polygonstyle_xml.tpl';
+	$styleHashName = 'polygonstyleInfo';
 	
 }elseif( !empty( $_REQUEST['polystyle_type'] ) ){
 	
@@ -30,20 +32,16 @@ if( @BitBase::verifyId( $_REQUEST['polylinestyle_id'] )) {
 		$getStyleListFunc = 'getPolygonStyles';
 	}
 
-	$gBitSmarty->assign( 'polystyle_type', $_REQUEST['polystyle_type'] );
-
 }
-
-$gBitSmarty->assign( 'polytype', $_REQUEST['polytype'] );
 
 // get one
 if( @BitBase::verifyId( $_REQUEST['polylinestyle_id'] ) || @BitBase::verifyId( $_REQUEST['polygonstyle_id'] ) ){
 	$XMLContent = "";
 	$statusCode = 401;
 
-	if( $result = $getStyleFunc( $styleId )) {
+	if( $result = $gContent->$getStyleFunc( $styleId )) {
 		$statusCode = 200;
-		$gBitSmarty->assign_by_ref( 'styleInfo', $result );
+		$gBitSmarty->assign_by_ref( $styleHashName, $result );
 	}else{
 		$XMLContent = "Requested Style Not Found";
 	}
@@ -60,6 +58,10 @@ if( @BitBase::verifyId( $_REQUEST['polylinestyle_id'] ) || @BitBase::verifyId( $
 	$gBitSmarty->assign( 'styles', $styles );
 
 	$gBitSmarty->assign( 'listInfo', $_REQUEST['listInfo'] );
+
+	$gBitSmarty->assign( 'polystyle_type', $_REQUEST['polystyle_type'] );
+
+	$gBitSmarty->assign( 'polytype', $_REQUEST['polytype'] );
 
 	$gBitSmarty->display( 'bitpackage:gmap/view_polystyles_inc.tpl', tra( 'Map' ) );
 }
