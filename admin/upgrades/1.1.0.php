e6ed3b522494dbb4afb1b46b83d388dca0de3d1f
Tekimaki <tekimaki_admin@users.sourceforge.net>
2009-02-26 18:22:53 +0000

    oops nicked nested arrays by accident - restore

--- admin/upgrades/1.1.0.php
+++ admin/upgrades/1.1.0.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_gmap/admin/upgrades/1.1.0.php,v 1.1 2009/02/26 18:19:08 tekimaki_admin Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_gmap/admin/upgrades/1.1.0.php,v 1.2 2009/02/26 18:22:53 tekimaki_admin Exp $
  */
 global $gBitInstaller;
 
@@ -9,7 +9,9 @@ $infoHash = array(
 	'version'      => str_replace( '.php', '', basename( __FILE__ )),
 	'description'  => "Reduce length of sequence fields for Firebird/Oracle.",
 );
-$gBitInstaller->registerPackageUpgrade( $infoHash, array( array( 'DATADICT' =>
+$gBitInstaller->registerPackageUpgrade( $infoHash, array( 
+
+array( 'DATADICT' => array(
 	array(
 		// Update sequence names
 		'RENAMESEQUENCE' => array(
@@ -18,9 +20,8 @@ $gBitInstaller->registerPackageUpgrade( $infoHash, array( array( 'DATADICT' =>
 			"gmaps_marker_styles_style_id_seq" => "gmaps_marker_style_id_seq",
 			"gmaps_polyline_styles_style_id_seq" => "gmaps_polyline_style_id_seq",
 			"gmaps_polygon_styles_style_id_seq" => "gmaps_polygon_style_id_seq",
-		),
-	),
-),
+	)),
+)),
 
 ));
 ?>
