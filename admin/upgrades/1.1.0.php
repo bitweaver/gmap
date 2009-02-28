c4c6ffcb331488b6f1d0d9b04f28fe25ad27a2b0
Tekimaki <tekimaki_admin@users.sourceforge.net>
2009-02-28 03:32:10 +0000

    tidy

--- admin/upgrades/1.1.0.php
+++ admin/upgrades/1.1.0.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_gmap/admin/upgrades/1.1.0.php,v 1.2 2009/02/26 18:22:53 tekimaki_admin Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_gmap/admin/upgrades/1.1.0.php,v 1.3 2009/02/28 03:32:10 tekimaki_admin Exp $
  */
 global $gBitInstaller;
 
@@ -12,14 +12,13 @@ $infoHash = array(
 $gBitInstaller->registerPackageUpgrade( $infoHash, array( 
 
 array( 'DATADICT' => array(
-	array(
-		// Update sequence names
-		'RENAMESEQUENCE' => array(
-			"gmaps_tilelayers_tilelayer_id_seq" => "gmaps_tilelayer_id_seq",
-			"gmaps_copyrights_copyright_id_seq" => "gmaps_copyright_id_seq",
-			"gmaps_marker_styles_style_id_seq" => "gmaps_marker_style_id_seq",
-			"gmaps_polyline_styles_style_id_seq" => "gmaps_polyline_style_id_seq",
-			"gmaps_polygon_styles_style_id_seq" => "gmaps_polygon_style_id_seq",
+	// Update sequence names
+	array( 'RENAMESEQUENCE' => array(
+		"gmaps_tilelayers_tilelayer_id_seq" => "gmaps_tilelayer_id_seq",
+		"gmaps_copyrights_copyright_id_seq" => "gmaps_copyright_id_seq",
+		"gmaps_marker_styles_style_id_seq" => "gmaps_marker_style_id_seq",
+		"gmaps_polyline_styles_style_id_seq" => "gmaps_polyline_style_id_seq",
+		"gmaps_polygon_styles_style_id_seq" => "gmaps_polygon_style_id_seq",
 	)),
 )),
 
